29486d0cb6be936b22eadba384d2fe2ce6e1ffe3
andrii.reva <elf1205@gmail.com>
2023-11-09 20:26:46 +0200

    initial commit restaurant

---
+++ src/main/java/com/example/javapractice/restaurant/RestaurantApplication.java
@@ -0,0 +1,14 @@
+package com.example.javapractice.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication - annotaion is entry to point to Spring Boot Application
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
